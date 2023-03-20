a9829f8712edd1d61a2fc4e50daac33a202e20c2
Nyamosiyego <edmond.nyamosi@strathmore.edu>
2023-03-21 00:11:12 +0300

    .

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
+const User = require("./models/user");
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -41,10 +43,26 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({
+    email: "colt@gmail.com",
+    username: "colt",
+  })
+  const newUser = await User.register(user, "chicken");
+  res.send(newUser);
 });
 
 app.get("/", (req, res) => {
