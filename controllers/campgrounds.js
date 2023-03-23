407f2d94e589e929b09eeffa187b8b6be9adda62
Nyamosiyego <edmond.nyamosi@strathmore.edu>
2023-03-23 21:17:40 +0300

    added edit map location

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -50,7 +50,12 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { runValidators: true, new: true });
+    campground.geometry = geoData.body.features[0].geometry;
     const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     if (req.body.deleteImages) {
